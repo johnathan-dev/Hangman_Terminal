8a406710c02fea0ef6b3fa8b1dc82dd9879b32dc
Johnathan Davidow <JohnathanDavidow@gmail.com>
2020-12-04 12:13:08 -0700

    Pretty much initial commit

--- Hangman_Terminal.py
+++ Hangman_Terminal.py
@@ -4,8 +4,13 @@
     Visit https://johnathandavidow.000webhostapp.com for more.
 '''
 
+# For hiding the secret word
 import getpass
+
+# For dramatic pauses in the title
 import time
+
+# For the computer to choose a word
 import random
 
 # Welcome message
@@ -27,7 +32,7 @@ def checkState(game_state, word_array, word):
         gallows = open(r"resources/gallowsWin.txt")
         print(gallows.read())
         gallows.close()
-        print("You win. The word was: "+word)
+        print(f"You win. The word was: {word}")
         return False
 
     # Detects loss
@@ -35,39 +40,49 @@ def checkState(game_state, word_array, word):
         gallows = open(r"resources/gallows"+str(game_state)+".txt")
         print(gallows.read())
         gallows.close()
-        print("You lose. The word was: "+word)
+        print(f"You lose. The word was: {word}")
         return False
     
     return True
 
-# Game against another player
-def gameHuman(word):
+def playGame(word):
+    # So the screen doesn't feel too disorganized
+    edge = "-" * 100
+
+    # The state will determine weather or not the game should end
     game_state = 0;
+    
     running = True
+
+    # Initializes and array the size of the word
     word_array = ["_"] * len(word)
+
     guessed_characters = ""
+
     while(running):
+        print(edge)
         gallows = open(r"resources/gallows"+str(game_state)+".txt")
         print(gallows.read())
         gallows.close()
-        print("Guessed characters:"+guessed_characters)
+        print(f"Guessed characters:{guessed_characters}")
         choice = ord(input("1. Guess character\n2. Guess word\n"))-48
         if(choice <= 1):
             char = input("Character: ")[0]
+            print(edge)
             if(char in word):
                 for j in range(len(word)):
                     if(char == word[j]):
                         word_array[j] = char
-                        print(char+" was in the word")
-                        print("Chances remaining: "+str(game_state+7))
+                        print(f"{char} was in the word")
+                        print(f"Chances remaining: {str(7-game_state)}")
 
                 if(char not in guessed_characters):
-                    guessed_characters += " "+char
+                    guessed_characters += f" {char}"
             else:
-                print(char+" was not in the word")
-                print("Chances remaining: "+game_state+7)
+                print(f"{char} was not in the word")
+                print(f"Chances remaining: {str(7-game_state)}")
                 if(char not in guessed_characters):
-                    guessed_characters += " "+char
+                    guessed_characters += f" {char}"
                 game_state += 1;
             for i in word_array:
                 print(i, end="")
@@ -75,61 +90,35 @@ def gameHuman(word):
         else:
             guess = input("Input what you think the word is: ")
             if guess == word:
-                c = 0
-                while(c < len(word)):
+                for c in range(len(word)):
                     word_array[c] = word[c]
-                    c += 1
             else:
+                print(f"{guess} was not the word")
                 game_state += 1
 
         # Detects win
         running = checkState(game_state, word_array, word)
+
+# Game against another player
+def gameHuman(word):
+    playGame(word)
 
 
 # Game against the computer
 def gameCPU():
+    # Opens text file containing random words
     dictionary = open(r"resources/dictionary.txt")
+
+    # Adds each line to an array
     lines = dictionary.readlines()
+
+    # Chooses one of the lines at random
     word = random.choice(lines) 
-    game_state = 0;
-    running = True
-    word_array = ["_"] * (len(word)-1)
-    guessed_characters = ""
-    while(running):
-        gallows = open(r"resources/gallows"+str(game_state)+".txt")
-        print(gallows.read())
-        gallows.close()
-        print("Guessed characters:"+guessed_characters)
-        choice = ord(input("1. Guess character\n2. Guess word\n"))-48
-        if(choice <= 1):
-            char = input("Character: ")[0]
-            if(char in word):
-                for j in range(len(word)):
-                    if(char == word[j]):
-                        word_array[j] = char
 
-                if(char not in guessed_characters):
-                    guessed_characters += " "+char
-            else:
-                print(char+" was not in the word")
-                if(char not in guessed_characters):
-                    guessed_characters += " "+char
-                game_state += 1;
-            for i in word_array:
-                print(i, end="")
-            print()
-        else:
-            guess = input("Input what you think the word is: ")
-            if guess == word:
-                c = 0
-                while(c < len(word)):
-                    word_array[c] = word[c]
-                    c += 1
-            else:
-                game_state += 1
+    # Cuts off the new line character
+    word = word[:len(word)-1]
 
-        # Detects win
-        running = checkState(game_state, word_array, word)
+    playGame(word)
 
 
 # Setup the game
